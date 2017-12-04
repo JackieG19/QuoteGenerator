7a13989b8433f605c8902d9934cc285e877b2265
Jackie <jacgut19@gmail.com>
2017-12-04 18:31:23 +0000

    redo

--- script.js
+++ script.js
@@ -14,13 +14,27 @@ function newQuote() {
 var randomQuote = Math.floor(Math.random() * (myQuotes.words.length));
 document.getElementById('diplayText').innerHTML = myQuotes.words[randomQuote];
 
-$(document).ready(function() {
-var tweet =
- $(".share").onclick(function(event){randomQuote(); 
-    'https://twitter.com/intent/tweet?text=' + myQuotes.words[randomQuote];
+/*$(document).ready(function() {
+var share = document.getElementById('diplayText').innerHTML;
+var tweet = 
+  $(".share").onclick(function(event){randomQuote(); 
+  'https://twitter.com/intent/tweet?text=' + myQuotes.words[randomQuote];
      window.open(tweet); 
     });
-     
-});  
+  */
 }
 
+function twitter(){
+
+var share = document.getElementById('diplayText').innerHTML;
+var tweet = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(share) + "&original_referer=";
+     window.open(tweet); 
+}
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
